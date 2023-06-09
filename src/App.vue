f2e41e9b2e0234e776b812fca2c9667bd70d3793
cyrilmuchemi <cyrilmuchemi@gmail.com>
2023-06-09 14:51:55 +0300

    Complete wireframe design

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,41 @@
+<template>
+    <div id="app">
+    <h1>ToDo</h1>
+    <to-do-form @todo-added="addTodo" ></to-do-form>
+    <ul>
+    <li v-for="item in TodoItems" :key="item.id">
+      <to-do-item :label="item.label" :done=item.done :id="item.id"></to-do-item>
+      <hr/>
+    </li>
+  </ul>
+  </div>
+</template>
+
 <script>
 import ToDoItem from "./components/TodoItem.vue";
+import ToDoForm from "./components/TodoForm.vue";
+import uniqueId from "lodash.uniqueid";
 export default {
   name: "app",
   components: {
-    ToDoItem
+    ToDoItem, ToDoForm
+  },
+  data(){
+    return {
+      TodoItems: [
+        {id:uniqueId('todo-'), label: "Learn Vue", done: false},
+        {id:uniqueId('todo-'), label: "builld a network", done: false},
+        {id:uniqueId('todo-'), label: "Drink coffee", done: true}
+      ],
+    };
+  },
+  methods: {
+    addTodo(toDoLabel){
+      this.TodoItems.push({id:uniqueId("todo-"), label:toDoLabel, done: false })
+    }
   }
 
 }
 </script>
 
-<template>
-    <div id="app">
-    <h1>To-Do List</h1>
-    <ul>
-    <li>
-      <to-do-item label="My ToDo Item" :done="true" ></to-do-item>
-    </li>
-  </ul>
-  </div>
-</template>
 
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <form @submit.prevent="onSubmit" >
+    <label for="new-todo-input"></label>
+    <input
+      type="text"
+      id="new-todo-input"
+      name="new-todo"
+      autocomplete="off" 
+      placeholder="Create a new todo..."
+      v-model.lazy.trim="label"
+      />
+  </form>
+</template>
+
+<script>
+  export default {
+    methods: {
+        onSubmit(){
+            if (this.label === "") {
+                return;
+        }
+            this.$emit("todo-added", this.label);
+            this.label = "";
+        }
+    },
+    data(){
+        return{
+            label: ""
+        }
+    }
+  };
+</script>
