9134aa959f74113ea89dace5418530474ece7b7b
cyrilmuchemi <cyrilmuchemi@gmail.com>
2023-06-09 16:30:03 +0300

    Add Tailwind utility classes & start styling

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
-    <div id="app">
-    <h1>ToDo</h1>
+    <div id="app" class="flex items-center justify-center bg-mobile-light md:bg-desktop-light font-Josefin">
+    <div class="text-center"> 
+      <div class="flex justify-between">
+        <h1 class="text-4xl text-white uppercase tracking-widest" >Todo</h1>
+        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fill-rule="evenodd" d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z"/></svg>
+      </div>
     <to-do-form @todo-added="addTodo" ></to-do-form>
+    <div class="absolute mt-5">
     <ul>
     <li v-for="item in TodoItems" :key="item.id">
       <to-do-item :label="item.label" :done=item.done :id="item.id"></to-do-item>
       <hr/>
     </li>
   </ul>
+      </div>
+    </div> 
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './public/**/*.html',
-    './src/**/*.{js,jsx,ts,tsx,vue}',
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'mobile-light' : "url('./src/assets/bg-mobile-light.jpg')",
+        'mobile-dark' : "url('./src/assets/bg-mobile-dark.jpg')",
+        'desktop-light' : "url('./src/assets/bg-desktop-light.jpg')",
+        'desktop-dark' : "url('./src/assets/bg-desktop-light.jpg')",
+      },
+      colors: {
+        'bright-blue': "hsl(220, 98%, 61%)",
+        'light-gray': "hsl(0, 0%, 98%)",
+        'light-grayish-blue': "hsl(236, 33%, 92%)",
+        'grayish-blue': "hsl(233, 11%, 84%)",
+        'dark-grayish-blue': "hsl(236, 9%, 61%)",
+        'very-dark-grayish-blue': "hsl(235, 19%, 35%)",
+        "check-background": "linear-gradient hsl(192, 100%, 67%)"
+      }
+    },
+    fontFamily: {
+      Josefin: ["Josefin Sans, sans-serif"]
+    }
+
   },
   plugins: [],
 }
